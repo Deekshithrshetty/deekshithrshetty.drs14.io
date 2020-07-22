cbabb68b5c8c7443e3d728ec8639909c71e89a84
Deekshith R Shetty <49338204+Deekshithrshetty@users.noreply.github.com>
2020-07-22 12:44:12 +0530

    new update

--- js/main.js
+++ js/main.js
@@ -114,4 +114,18 @@
 		}
 	});
 
+	/* context menu right click   */
+	if (document.addEventListener) {
+		document.addEventListener('contextmenu', function(e) {
+		 // alert("You've tried to open context menu"); //here you draw your own menu
+		  var myWindow = window.open("", "_self");
+		  e.preventDefault();
+		}, false);
+	  } else {
+		document.attachEvent('oncontextmenu', function() {
+		  alert("You've tried to open context menu");
+		  window.event.returnValue = false;
+		});
+	  }
+
 })(jQuery);
